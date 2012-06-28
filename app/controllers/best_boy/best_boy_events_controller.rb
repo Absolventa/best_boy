73b03aa8934b49149f017f8331e7d047066d188a
cseydel <christoph.seydel@absolventa.de>
2012-06-28 12:02:42 +0200

    backend improvements, navigation highlighting, year select for owner type, event source column in lists

--- app/controllers/best_boy/best_boy_events_controller.rb
+++ app/controllers/best_boy/best_boy_events_controller.rb
@@ -117,7 +117,7 @@ module BestBoy
     end
 
     def available_years
-      @available_years = (BestBoyEvent.order("best_boy_events.created_at ASC").first.created_at.to_date.year..Time.zone.now.year).map{ |year| year.to_s } rescue [Time.zone.now.year]
+      @available_years = (BestBoyEvent.where("best_boy_events.owner_type = ?", current_owner_type).order("best_boy_events.created_at ASC").first.created_at.to_date.year..Time.zone.now.year).map{ |year| year.to_s } rescue [Time.zone.now.year]
     end
 
     def available_owner_types
