ca61a7f3703bf41f09ab2424b706e09655e64757
cseydel <christoph.seydel@absolventa.de>
2013-09-25 17:47:58 +0200

    add skip_after-filter to best_boy admin layout

--- lib/best_boy.rb
+++ lib/best_boy.rb
@@ -3,13 +3,14 @@ require "best_boy/engine.rb"
 module BestBoy
   # Define ORM
   mattr_accessor :precompile_assets, :orm, :base_controller, :before_filter,
-                 :skip_before_filter, :custom_redirect
+                 :skip_before_filter, :skip_after_filter, :custom_redirect
 
   @@precompile_assets = false
   @@orm = :active_record
   @@base_controller = "ApplicationController"
   @@before_filter = nil
   @@skip_before_filter = nil
+  @@skip_after_filter = nil
   @@custom_redirect = nil
 
   # Load configuration from initializer
--- lib/generators/templates/best_boy.rb
+++ lib/generators/templates/best_boy.rb
@@ -14,6 +14,9 @@ BestBoy.setup do |config|
   # Define skip_before_filter for skipping before_filters (default = nil)
   #config.skip_before_filter = nil
 
+  # Define skip_after_filter for skipping after_filters (default = nil)
+  #config.skip_after_filter = nil
+
   # Define custom redirect path as string p.e. "/admin" (default = nil)
   #config.custom_redirect = nil
 end
