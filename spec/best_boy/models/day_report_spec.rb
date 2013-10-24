73e09a1797135d3cebfad68298af3f6fc15047a0
Robin Neumann <robin.neumann@absolventa.de>
2013-10-24 11:39:16 +0200

    add specs for reports

---
+++ spec/best_boy/models/day_report_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe BestBoy::DayReport do
+
+  let(:eventable) { Example.create }
+  let(:month_report) do
+    BestBoy::MonthReport.create({
+      eventable_id:   eventable.id,
+      eventable_type: eventable.class.to_param
+      event_type:     "create"
+    })
+  end
+  let(:day_report) do
+     BestBoy::DayReport.create({
+      eventable_id:   eventable.id,
+      eventable_type: eventable.class.to_param
+      event_type:     "create"
+    })
+  end
+
+  subject { day_report }
+
+  context "with associations" do
+    it { should belong_to(:month_report) }
+  end
+
+  context "with validations" do
+    it "validates presence of attributes" do
+      should validate_presence_of(:month_report_id)
+      should validate_presence_of(:eventable_type)
+      should validate_presence_of(:eventable_id)
+      should validate_presence_of(:event_type)
+    end
+  end
+
+  context "with scopes" do
+    it "aggregates DayReports of specific day" do
+      collection = BestBoy::DayReport.order('created_at DESC')
+      expect(collection.created_on(Time.zone.now)).to include(day_report)
+      expect(collection.created_on(1.day.ago)).to_not include(day_report)
+    end
+  end
+
+  context "with instance methods" do
+    context "with delegations" do
+      its(:day) { should == subject.created_at.day }
+      its(:month) { should == subject.created_at.month }
+      its(:year) { should == subject.created_at.year }
+    end
+
+    describe "#closed?" do
+      it "is not closed if it is the youngest DayReport for the eventable" do
+        expect(day_report.closed?).to be_false
+      end
+
+      it "is closed if it is not the youngest DayReport" do
+        newer_day_report = BestBoy::DayReport.new(
+          eventable_type: day_report.eventable_type,
+          eventable_id: day_report.eventable_id,
+          month_report_id: month_report.to_param,
+          event_type: day_report.event_type)
+        newer_day_report.save
+        expect(day_report.closed?).to be_true
+      end
+    end
+  end
+
+  context "with class methods" do
+    describe "#create_for" do
+      report = BestBoy::DayReport.create_for(eventable)
+
+      it { expect(report).to be_valid }
+      it { expect(report.eventable_id).to be_eql(eventable.id) }
+      it { expect(report.eventable_type).to be_eql(eventable.class.to_s) }
+    end
+
+    describe "#current_for" do
+      context "when day_report exists" do
+        existing_report = BestBoy::DayReport.create_for(eventable, "create")
+        demanded = BestBoy::DayReport.current_for(eventable, "create")
+        it { expect(demanded).to be_eql existing_report }
+      end
+
+      context "when no today's day_report is present" do
+        eventable = Example.create # important to be placed right her # important to be placed right heree
+        scope = BestBoy::DayReport.where(eventable_id: eventable.id, eventable_type: eventable.class.to_s, event_type: "create")
+        it { expect(scope.today).to be_empty }
+        it { expect{ BestBoy::DayReport.current_for(eventable, "create") }.to change(scope.today, :count).by(1) }
+      end
+    end
+  end
+end
+
---
+++ spec/best_boy/models/month_report_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+describe BestBoy::MonthReport do
+
+  let(:eventable) { Example.create }
+  let(:month_report) do
+    BestBoy::MonthReport.create({
+      eventable_id:   eventable.id,
+      eventable_type: eventable.class.to_param
+      event_type:     "create"
+    })
+  end
+
+  subject { month_report }
+
+  describe "with associations" do
+    it { should have_many(:day_reports) }
+  end
+
+  describe "with validations" do
+    it "should validate presence of attributes" do
+      should validate_presence_of(:eventable_type)
+      should validate_presence_of(:eventable_id)
+      should validate_presence_of(:event_type)
+    end
+  end
+
+  context "with scopes" do
+    it "aggregates MonthReports of specific month" do
+      collection = BestBoy::MonthReport.order('created_at DESC')
+      expect(collection.month(Time.zone.now.month, Time.zone.now.year)).to include(month_report)
+      expect(collection.month(2.month.ago.month, 2.month.ago.year )).to_not include(month_report)
+    end
+    it "aggregates MonthReports of specific day" do
+      collection = BestBoy::MonthReport.order('created_at DESC')
+      expect(collection.created_on(Time.zone.now)).to include(month_report)
+      expect(collection.created_on(1.day.ago)).to_not include(month_report)
+    end
+  end
+
+  context "with instance methods" do
+    context "with delegations" do
+      its(:month) { should == subject.created_at.month }
+      its(:year) { should == subject.created_at.year }
+    end
+
+    describe "#closed?" do
+      it "is not closed if no younger MonthReport for the eventable_id exists" do
+        expect(month_report.closed?).to be_false
+      end
+
+      it "is closed if it is not the youngest MonthReport" do
+        newer_month_report = BestBoy::MonthReport.new(eventable_type: month_report.eventable_type, eventable_id: month_report.eventable_id, event_type: month_report.event_type)
+        newer_month_report.save
+        expect(month_report.closed?).to be_true
+      end
+    end
+  end
+
+  context "with class methods" do
+    describe "#create_for" do
+      report = BestBoy::MonthReport.create_for(eventable, "create")
+
+      it { expect(report).to be_valid }
+      it { expect(report.eventable_id).to be_eql(eventable.id) }
+      it { expect(report.eventable_type).to be_eql(eventable.class.to_s) }
+      it { expect(report.event_type).to be_eql("create") }
+    end
+
+    describe "#current_for" do
+      context "when month_report exists" do
+        existing_report = BestBoy::MonthReport.create_for(eventable, "create")
+        demanded = BestBoy::MonthReport.current_for(eventable, "create")
+        it { expect(demanded).to be_eql existing_report }
+      end
+
+      context "when no today's day_report is present" do
+        eventable = Example.create # important to be placed right her # important to be placed right heree
+        scope = BestBoy::MonthReport.where(eventable_id: eventable.id, eventable_type: eventable.class.to_s, event_type: "create")
+        mth = Time.zone.now.month
+        yr = Time.zone.now.year
+
+        it { expect(scope.month(mth, yr)).to be_empty }
+        it { expect{ BestBoy::MonthReport.current_for(viewable) }.to change(scope.month(mth, yr), :count).by(1) }
+      end
+    end
+  end
+end
