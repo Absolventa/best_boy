759d8cef79befea0bdc724e04785890fe2a167fa
Robin Neumann <robin.neumann@absolventa.de>
2013-10-24 10:54:25 +0200

    add new migrations to generator

--- lib/generators/active_record/best_boy_generator.rb
+++ lib/generators/active_record/best_boy_generator.rb
@@ -14,7 +14,7 @@ module ActiveRecord
       end
 
       def create_migration_file
-        %w(create_best_boy_events_table.rb add_event_source_to_best_boy_events_table.rb).each do |migration_file|
+        %w(create_best_boy_events_table.rb add_event_source_to_best_boy_events_table.rb create_reports.rb).each do |migration_file|
           destination = "db/migrate/#{migration_file.sub(%r\.rb$\, '')}"
           if not self.class.migration_exists?(File.dirname(destination), File.basename(destination))
             migration_template migration_file, destination
