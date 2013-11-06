77e2a60f671d6ea8b55c9f631bce1a87e0149d80
Robin Neumann <robin.neumann@absolventa.de>
2013-11-06 09:36:31 +0100

    minor fix: Feb 2012 had 29 days, so let there be 29 days printed on the axis, too

--- app/controllers/best_boy/best_boy_events_controller.rb
+++ app/controllers/best_boy/best_boy_events_controller.rb
@@ -12,7 +12,7 @@ module BestBoy
     helper BestBoy::BestBoyViewHelper
     helper_method :available_owner_types, :available_events, :available_event_sources, :available_years,
                   :current_owner_type, :current_event, :current_event_source, :current_month, :current_year, :collection,
-                  :render_chart, :month_name_array, :detail_count
+                  :days_of, :render_chart, :month_name_array, :detail_count
 
     def stats
       collect_occurrences
