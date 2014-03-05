d9c8b3eea80bdfea0459787516210407d7ad4322
Carsten Zimmermann <carp@hacksocke.de>
2014-03-05 11:48:03 +0100

    Add shared example

---
+++ spec/support/shared/obeys_test_mode.rb
@@ -0,0 +1,25 @@
+shared_examples 'Short-circuits saving in test mode' do
+  subject { described_class.new }
+
+  describe '#save' do
+    it 'does not create a record in test mode' do
+      subject.stub(:valid?).and_return(true)
+      BestBoy.in_test_mode do
+        expect do
+          expect(subject.save).to eql true
+        end.not_to change { subject.class.count }
+      end
+    end
+  end
+
+  describe '#save!' do
+    it 'does not create a record in test mode' do
+      subject.stub(:valid?).and_return(true)
+      BestBoy.in_test_mode do
+        expect do
+          expect(subject.save!).to eql true
+        end.not_to change { subject.class.count }
+      end
+    end
+  end
+end
