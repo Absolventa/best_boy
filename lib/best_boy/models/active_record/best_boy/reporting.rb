d99579285260957139e75f11941246a13a2854ae
Robin Neumann <robin.neumann@absolventa.de>
2015-01-19 16:39:38 +0100

    Remove method duplicate

--- lib/best_boy/models/active_record/best_boy/reporting.rb
+++ lib/best_boy/models/active_record/best_boy/reporting.rb
@@ -14,19 +14,6 @@ module BestBoy
           report.present? ? report : create_for(owner, type, source, date)
         end
 
-        def create_for(owner, type, source = nil, date = Time.zone.now)
-          month_report = BestBoy::MonthReport.current_or_create_for(owner, type, source, date)
-          day_report   = BestBoy::DayReport.new
-
-          day_report.owner_type      = owner
-          day_report.event           = type
-          day_report.month_report_id = month_report.id
-          day_report.event_source    = source
-          day_report.created_at      = date
-
-          day_report.save ? day_report : nil
-        end
-
         def for(owner, type, source = nil)
           where(owner_type: owner, event: type, event_source: source)
         end
