68b8f67375d48642527b9f164a126b6ec3525ffe
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2015-06-10 17:00:55 +0200

    New routing system

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ Changes in version 3
 Best Boy version 3 comes with some incompatible backwards changes. However, it's totally worth it because BestBoy now follows best practices of a rails eninge.
 This and additional changes of version 3 includes
 
+* Uses rails routing convention instead of named routes
 * serving the necessary migrations from the engines migration folder
 * serving all assets via the asset pipeline (used to be a opt-in before)
 * removes unnecessary generator boilerplate and config options
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
-Rails.application.routes.draw do
-  get "best_boy_admin" => "best_boy/best_boy_events#index", :as => :best_boy_admin
-  get "best_boy_admin/stats" => "best_boy/best_boy_events#stats", :as => :best_boy_admin_stats
-  get "best_boy_admin/lists" => "best_boy/best_boy_events#lists", :as => :best_boy_admin_lists
-  get "best_boy_admin/charts" => "best_boy/best_boy_events#charts", :as => :best_boy_admin_charts
-  get "best_boy_admin/details" => "best_boy/best_boy_events#details", :as => :best_boy_admin_details
-  get "best_boy_admin/monthly_details" => "best_boy/best_boy_events#monthly_details", :as => :best_boy_admin_monthly_details
+BestBoy::Engine.routes.draw do
+
+  root to: "best_boy_events#index"
+
+  resources :best_boy_events, only: [:index] do
+    collection do
+      get :stats
+      get :list
+      get :charts
+      get :details
+      get :monthly_details
+    end
+  end
+
+  # get "best_boy_admin" => "best_boy/best_boy_events#index", :as => :best_boy_admin
+  # get "best_boy_admin/stats" => "best_boy/best_boy_events#stats", :as => :best_boy_admin_stats
+  # get "best_boy_admin/lists" => "best_boy/best_boy_events#lists", :as => :best_boy_admin_lists
+  # get "best_boy_admin/charts" => "best_boy/best_boy_events#charts", :as => :best_boy_admin_charts
+  # get "best_boy_admin/details" => "best_boy/best_boy_events#details", :as => :best_boy_admin_details
+  # get "best_boy_admin/monthly_details" => "best_boy/best_boy_events#monthly_details", :as => :best_boy_admin_monthly_details
 end
